5c15142bcb6c71bd9167cfbe8b217b98021a048f
ahmed <bayt@hotmail.de>
2018-09-30 09:19:56 +0100

    add components and jwt plugin

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ResetRequestComponent } from './components/password/reset-request/reset-request.component';
+import { ResetResponseComponent } from './components/password/reset-response/reset-response.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    ResetRequestComponent,
+    ResetResponseComponent
   ],
   imports: [
     BrowserModule
